ccc880ea83dc12d6d9a53b7f83a72aabcde75e17
abhi-si <92526811+abhi-si@users.noreply.github.com>
2024-10-18 13:23:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1>hello</h1>
+      <Button>wow</Button>
+     
+
+    </>
+  )
+}
+
+export default App
